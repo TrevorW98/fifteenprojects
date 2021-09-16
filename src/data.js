134229b27a87192684983a57371f3a31afb050cc
Trevor Womack Jr <Twomack6823@gmail.com>
2021-09-16 11:43:04 -0700

    Removed unneccesary files, began setup

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: 1,
+        name: 'Trevor Womack',
+        age: 23,
+        image: '../images/bday1.jpg'
+    },
+    {
+        id: 1,
+        name: 'SJ Giese',
+        age: 23,
+        image: '../images/bday1.jpg'
+    },
+    {
+        id: 1,
+        name: 'Jacob Zigzagerous',
+        age: 27,
+        image: '../images/bday1.jpg'
+    },
+    {
+        id: 1,
+        name: 'Robert Mandrews',
+        age: 20,
+        image: '../images/bday1.jpg'
+    },
+    {
+        id: 1,
+        name: 'Xavier Birdins',
+        age: 25,
+        image: '../images/bday1.jpg'
+    },
+
+]
