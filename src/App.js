3a55f92c358e3a5d7c175b23535498df4f3258be
Trevor Womack Jr <Twomack6823@gmail.com>
2021-09-17 17:44:52 -0700

    birthday list complete

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import data from './data';
 import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className='container'>
-        <h3>0 Birthdays today</h3>
-        <List></List>
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people}/>
         <button onClick={() => console.log('clicked')}>
           clear all
         </button>
--- src/data.js
+++ src/data.js
@@ -3,31 +3,31 @@ export default [
         id: 1,
         name: 'Trevor Womack',
         age: 23,
-        image: '../images/bday1.jpg'
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'
     },
     {
         id: 1,
         name: 'SJ Giese',
         age: 23,
-        image: '../images/bday1.jpg'
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg'
     },
     {
         id: 1,
         name: 'Jacob Zigzagerous',
         age: 27,
-        image: '../images/bday1.jpg'
+        image:  'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg'
     },
     {
         id: 1,
         name: 'Robert Mandrews',
         age: 20,
-        image: '../images/bday1.jpg'
+        image:  'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg'
     },
     {
         id: 1,
         name: 'Xavier Birdins',
         age: 25,
-        image: '../images/bday1.jpg'
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg'
     },
 
 ]
